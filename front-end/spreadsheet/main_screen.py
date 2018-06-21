feca9377f308d71f6993556232524c6245ebf2b1
Mark Lockett <mark@deliveryengine.net>
2018-06-21 18:31:03 +0800

    Poll the server for edges in the public document, ignore any that we already have

--- front-end/spreadsheet/main_screen.py
+++ front-end/spreadsheet/main_screen.py
@@ -6,14 +6,18 @@ except ImportError:
 from cavorite.HTML import *
 from . import myglobals
 from cavorite.ajaxget import ajaxpost, ajaxget
+from cavorite.timeouts import set_interval
 
-public_sheets = None
+public_sheets_loader_running = False
+downloaded_public_sheet_edges = set()
 
 class MainScreen(div):
     def __init__(self, owner, *args, **kwargs):
         self.owner = owner
         super(MainScreen, self).__init__(*args, **kwargs)
-        if public_sheets is None:
+        global public_sheets_loader_running
+        if public_sheets_loader_running is False:
+            set_interval(self.load_public_sheets, 5000)
             self.load_public_sheets()
 
     def get_children(self):
@@ -29,22 +33,31 @@ class MainScreen(div):
         attribs['class'] = 'main-screen'
         return attribs
 
+    def public_sheet_edge_downloaded(self, xmlhttp, response, key):
+        #print('public_sheet_edge_downloaded called key=', key)
+        global downloaded_public_sheet_edges
+        downloaded_public_sheet_edges.add(key)
+
     def public_sheets_result_handler(self, xmlhttp, response):
         if xmlhttp.status >= 200 and xmlhttp.status <= 299:
-            print('Public sheet list retreived', ' self=', self, ', xmlhttp.responseText=', xmlhttp.responseText, ' response=', response)
-            global public_sheets
-            public_sheets = str(xmlhttp.responseText)
+            #print('public_sheets_result_handler called')
+            #print('Public sheet list retreived', ' self=', self, ', xmlhttp.responseText=', xmlhttp.responseText, ' response=', response)
+            global downloaded_public_sheet_edges
             # These XML fragments are Javascript objects so we need to navigate them with Javascript like constructs
             l = response.getElementsByTagName("Contents")
-            print('l=', l)
             for i in range(l.length):
-                print('i=', i)
                 key_element = l[i].getElementsByTagName("Key")[0]
-                print('key_elemen=', key_element)
                 key = str(key_element.childNodes[0].nodeValue)
-                print('key=', key)
+                #print('Found key = ', key)
+                if key not in downloaded_public_sheet_edges:
+                    ajaxget('/storage/public/' + key, lambda xmlhttp, response, key=key: self.public_sheet_edge_downloaded(xmlhttp, response, key))
+                else:
+                    pass
+                    #print('key=', key, ' already downloaded skippign')
             self.owner.mount_redraw()
 
     def load_public_sheets(self):
+        global public_sheets_loader_running
+        public_sheets_loader_running = True
         ajaxget('/storage/public/', self.public_sheets_result_handler)
 
